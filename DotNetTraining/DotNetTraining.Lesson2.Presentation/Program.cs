096aae03394996337347f26bce94e4979ebf6608
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-01-22 10:35:50 +0300

    Init

---
+++ DotNetTraining/DotNetTraining.Lesson2.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetTraining.Lesson2.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
