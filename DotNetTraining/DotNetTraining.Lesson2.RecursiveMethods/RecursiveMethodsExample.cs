736f46549a5356737e29b2bda83d589addcf1353
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-01-22 15:32:33 +0300

    Added recursive methods example

---
+++ DotNetTraining/DotNetTraining.Lesson2.RecursiveMethods/RecursiveMethodsExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DotNetTraining.Lesson2.RecursiveMethods
+{
+    public static class RecursiveMethodsExample
+    {
+        public static long ComputeFactorial(this int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Argument should be positive.", nameof(n));
+            }
+            
+            return n >= 1 ? n * ComputeFactorial(n - 1) : 1;
+        }
+    }
+}
