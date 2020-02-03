b5fee48ad57a09601478e66d87aec9ff3edbb296
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-03 20:33:16 +0300

    Added dotnet delegate example

---
+++ DotNetTraining/DotNetTraining.Lesson6.Delegates/DotNetDelegateExamples.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetTraining.Lesson6.Delegates
+{
+    public class DotNetDelegateExamples
+    {
+        private List<int> _values;
+
+        public DotNetDelegateExamples(List<int> values)
+        {
+            _values = values;
+        }
+
+        public List<int> FilterBadPractice(Predicate<int> filterExpression)
+        {
+            var filteredValues = new List<int>();
+
+            foreach (var value in _values)
+            {
+                if (filterExpression(value))
+                {
+                    filteredValues.Add(value);
+                }
+            }
+
+            return filteredValues;
+        }
+
+        #region Best Practice 
+
+        public IEnumerable<int> Filter(Predicate<int> filterExpression) => _values.Where(x => filterExpression(x));
+
+        public IEnumerable<int> Filter(Func<int, bool> filterExpression) => _values.Where(filterExpression);
+
+        #endregion
+    }
+}
