089565e10143175d970963dc988596c750353f96
Eugeny Kalugin <t-ezk@ersl.ie>
2020-02-03 11:46:29 +0300

    lesson 1 initial commit

---
+++ DotNetTraining/DotNetTraining.Lesson1.LoopsAndArrays/LoopsAndArraysExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetTraining.Lesson1.LoopsAndArrays
+{
+    public class LoopsAndArraysExample
+    {
+	    public void PrintLoopsExample()
+	    {
+		    for (int i = 0; i < 5; i++)
+		    {
+			    Console.WriteLine(i);
+		    }
+
+		    int n = 0;
+		    while (n < 5)
+		    {
+			    Console.WriteLine(n);
+			    n++;
+		    }
+
+		    int x = 0;
+		    do
+		    {
+			    Console.WriteLine(x);
+			    x++;
+		    } while (x < 5);
+
+		    var fibNumbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13 };
+		    int count = 0;
+		    foreach (int element in fibNumbers)
+		    {
+			    count++;
+			    Console.WriteLine($"Element #{count}: {element}");
+		    }
+		    Console.WriteLine($"Number of elements: {count}");
+		}
+
+	    public void PrintArraysExample()
+	    {
+
+	    }
+    }
+}
---
+++ DotNetTraining/DotNetTraining.Lesson1.Presentation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTraining.Lesson1.Presentation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
