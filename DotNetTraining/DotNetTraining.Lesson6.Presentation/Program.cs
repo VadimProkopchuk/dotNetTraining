5193ade2e3597dd35ed68d969e21baca3508ad47
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-03 10:40:33 +0300

    Added delegate examples for lesson 6

---
+++ DotNetTraining/DotNetTraining.Lesson6.Presentation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DotNetTraining.Lesson6.Delegates;
+
+namespace DotNetTraining.Lesson6.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            RunDelegateExample();
+        }
+
+
+        static void RunDelegateExample()
+        {
+            var user = new User("Vadim");
+
+            user.PrintInfo(Console.WriteLine);
+            user.PrintInfo(delegate(string info) { Console.WriteLine("Anonymous function. {0}", info); });
+            user.PrintInfo(info => Console.WriteLine("Anonymous function. {0}", info));
+
+            
+            Console.WriteLine("========");
+            
+            Print print = Console.WriteLine;
+            print += info => Console.WriteLine("2. {0}", info);
+            print += info => Console.WriteLine("3. {0}", info);
+
+            user.PrintInfo(print);
+        }
+    }
+}
