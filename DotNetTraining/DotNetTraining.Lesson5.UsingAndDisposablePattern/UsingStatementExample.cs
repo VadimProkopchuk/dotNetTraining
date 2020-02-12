417198e6b1627e45f33fc5303fa4b9bb6c116565
Eugeny Kalugin <t-ezk@ersl.ie>
2020-02-12 11:23:20 +0300

    added lesson 5 examples

---
+++ DotNetTraining/DotNetTraining.Lesson5.UsingAndDisposablePattern/UsingStatementExample.cs
@@ -0,0 +1,27 @@
+﻿namespace DotNetTraining.Lesson5.UsingAndDisposablePattern
+{
+	public class UsingStatementExample
+	{
+		public void RunUsingStatementExample()
+		{
+			using (var disposableClass = new DerivedDisposableClass())
+			{
+				// do some work
+			}
+		}
+
+		public void RunExpandedUsingStatementExample()
+		{
+			var baseClass = new BaseDisposableClass();
+
+			try
+			{
+				// do some work
+			}
+			finally
+			{
+				baseClass?.Dispose();
+			}
+		}
+	}
+}
