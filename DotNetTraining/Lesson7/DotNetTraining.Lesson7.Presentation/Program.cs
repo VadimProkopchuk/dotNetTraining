fba7cf6c5b91d28bb67a6e996ff699c0fa7cc592
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-13 12:00:38 +0300

    Lesson 7: empty projects

---
+++ DotNetTraining/Lesson7/DotNetTraining.Lesson7.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetTraining.Lesson7.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
