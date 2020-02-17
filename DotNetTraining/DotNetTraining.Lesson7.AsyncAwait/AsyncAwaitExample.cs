1413190e6104d7ef750f7d670fdcc6f87191758a
Eugeny Kalugin <t-ezk@ersl.ie>
2020-02-17 12:53:59 +0300

    added lesson 7: parts 1 and 3

---
+++ DotNetTraining/DotNetTraining.Lesson7.AsyncAwait/AsyncAwaitExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetTraining.Lesson7.AsyncAwait
+{
+	public class AsyncAwaitExample
+	{
+		public static async Task RunAsyncAwaitExampleAsync()
+		{
+			var cts = new CancellationTokenSource();
+			var token = cts.Token;
+			var numberOne = await FactorialValueTaskAsync(10);
+			var numberTwo = await FactorialAsync(5);
+
+			FactorialWhenAllAsync();
+		}
+
+		private static int Factorial(int n)
+		{
+			if (n < 1)
+			{
+				throw new Exception("Exception message");
+			}
+
+			var result = 1;
+
+			for (var i = 1; i <= n; i++)
+			{
+				result *= i;
+			}
+
+			return result;
+		}
+
+		private static async Task<int> FactorialAsync(int n)
+		{
+			return await Task.Run(() => Factorial(n));
+		}
+
+		private static async void FactorialWhenAllAsync()
+		{
+			try
+			{
+				Task taskOne = Task.Run(() => Factorial(4));
+				Task taskTwo = Task.Run(() => Factorial(3));
+				Task taskThree = Task.Run(() => Factorial(5));
+
+				await Task.WhenAll(taskOne, taskTwo, taskThree);
+
+			}
+			catch (Exception ex)
+			{
+				//handle ex
+			}
+
+		}
+
+		private static async ValueTask<int> FactorialValueTaskAsync(int n)
+		{
+			return await Task.Run(() => Factorial(n));
+		}
+	}
+}
---
+++ DotNetTraining/DotNetTraining.Lesson7.Thread/ThreadSynchronizationExample.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading;
+
+namespace DotNetTraining.Lesson7.Thread
+{
+	public class ThreadSynchronizationExample
+	{
+		private static int _sharedVariable;
+
+		private static readonly object Locker = new object();
+
+		private static readonly Mutex MutexObj = new Mutex();
+
+		private static readonly Semaphore Semaphore = new Semaphore(3, 3);
+
+		private static readonly AutoResetEvent WaitHandler = new AutoResetEvent(true);
+
+		public static void RunSynchronizationExample()
+		{
+			for (var i = 0; i < 5; i++)
+			{
+				var thread = new System.Threading.Thread(RunLockExample);
+
+				thread.Start();
+			}
+		}
+
+		private static void RunLockExample()
+		{
+			lock (Locker)
+			{
+				_sharedVariable = 1;
+				_sharedVariable++;
+			}
+		}
+
+		private static void RunMonitorExample()
+		{
+			var acquiredLock = false;
+
+			try
+			{
+				Monitor.Enter(Locker, ref acquiredLock);
+
+				_sharedVariable = 1;
+				_sharedVariable++;
+			}
+			finally
+			{
+				if (acquiredLock)
+				{
+					Monitor.Exit(Locker);
+				}
+			}
+		}
+
+		private static void RunMutexExample()
+		{
+			MutexObj.WaitOne();
+
+			_sharedVariable = 1;
+			_sharedVariable++;
+
+			MutexObj.ReleaseMutex();
+		}
+
+		private static void RunSemaphoreExample()
+		{
+			Semaphore.WaitOne();
+
+			_sharedVariable = 1;
+			_sharedVariable++;
+
+			Semaphore.Release();
+		}
+
+		private static void RunAutoResetEventExample()
+		{
+			WaitHandler.WaitOne();
+
+			_sharedVariable = 1;
+			_sharedVariable++;
+
+			WaitHandler.Set();
+		}
+	}
+}
