7c4b97631b6487b41642b3a5d4d2978a2f03feab
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-25 13:50:27 +0300

    Added api controller for lesson 9

---
+++ DotNetTraining/Lesson9/DotNetTraining.Lesson9.AspApplication/Controllers/UsersApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNetTraining.Lesson9.AspApplication.Services.Contracts;
+using DotNetTraining.Lesson9.AspApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTraining.Lesson9.AspApplication.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly IUsersService usersService;
+
+        public UsersApiController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await usersService.GetAsync();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var user = await usersService.GetAsync(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateUserViewModel model)
+        {
+            var user = await usersService.CreateAsync(model);
+            return Ok(user);
+        }
+    }
+}
