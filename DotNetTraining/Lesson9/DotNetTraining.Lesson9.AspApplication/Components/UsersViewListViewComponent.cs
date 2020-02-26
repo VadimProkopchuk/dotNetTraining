fe560b604782b8105b762cb90492781b5e504ad5
v.prokopchuk <vadim.prokopchuk@ocsico.com>
2020-02-26 11:04:13 +0300

    Added view component example (lesson 10)

---
+++ DotNetTraining/Lesson9/DotNetTraining.Lesson9.AspApplication/Components/UsersViewListViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using DotNetTraining.Lesson9.AspApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTraining.Lesson9.AspApplication.Components
+{
+    public class UsersViewListViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(List<UserViewModel> users)
+        {
+            return View(users);
+        }
+    }
+}
